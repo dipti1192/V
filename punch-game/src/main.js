8f24643d33b6bff26ab52971877fc76abab29f92
Dipti Prabhavale <dipti.prabhavale1192@gmail.com>
2017-12-20 19:32:57 +0530

    Added Vue Project

---
+++ punch-game/src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#vue-app',
+  router,
+  data: {
+    restart: false,
+    width: 200,
+    styleObj: {width: this.width + 'px'}
+  },
+  methods: {
+    punchBag: function () {
+      this.width -= 10
+      this.styleObj = {width: this.width + 'px'}
+      if (this.width <= 0) {
+        this.restart = true
+      }
+    },
+    restartGame: function () {
+      this.restart = false
+      this.styleObj = {width: 200 + 'px'}
+    }
+  }
+  // template: '<App/>',
+  // components: { App }
+})
