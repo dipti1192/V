42720d5975587011b0c00c7f9611a356f72d853f
Dipti Prabhavale <dipti.prabhavale1192@gmail.com>
2017-12-22 16:17:14 +0530

    Added Component

--- punch-game/src/App.vue
+++ punch-game/src/App.vue
@@ -1,23 +1,82 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header></header>
+    <div class="middle_section">
+      <div @click="user.enableDetails=!user.enableDetails" class="tiles" v-for="user in users">
+        <h2>{{user.name}}</h2>
+        <h5 v-show="user.enableDetails">{{user.designation}}</h5>
+      </div>
+    </div>
+    <footer>
+      <span class="copyrignt">@copyrignt 2017</span>
+    </footer>  
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    data () {
+      return{
+        users: [
+              {
+                name: 'Dipti',
+                designation: 'Sr.Software Engineer',
+                enableDetails: false
+              },
+              {
+                name: 'Pooja',
+                designation: 'Sr.Software Engineer',
+                enableDetails: false
+              },
+              {
+                name: 'Manali',
+                designation: 'Software Engineer',
+                enableDetails: false
+              }
+            ]
+      }
+    }
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #app {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  header{
+    background : lightgreen;
+    height: 15%;
+  }
+
+  footer{
+    height: 10%;
+    background: black;
+    text: lightgreen;
+    text-align: center;
+
+  }
+
+  .middle_section{
+    width: 100%;
+    height: 75%;
+    background: grey;
+  }
+
+  .copyrignt{
+    padding: 20px;
+  }
+
+  .tiles{
+    brorder: solid black 1px;
+    background: lightgreen;
+  }
+  
 </style>
