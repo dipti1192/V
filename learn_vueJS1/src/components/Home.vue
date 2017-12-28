3adbef9f205bd835c3aec097a37b9662797ef665
Dipti Prabhavale <dipti.prabhavale1192@gmail.com>
2017-12-28 18:59:55 +0530

    Added get call and dynamic rendering according to it

--- learn_vueJS1/src/components/Home.vue
+++ learn_vueJS1/src/components/Home.vue
@@ -9,37 +9,71 @@
 
 <script>
 import Section from './Section'
-// Vue.http.options.emulateJSON = true; 
+// Vue.http.options.emulateJSON = true;
 export default {
   name: 'Home',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      columns: [
-        {id: 1, name: 'A', text1: 'Dummy Text 1', text2: 'Dummy Text 2'},
-        {id: 2, name: 'B', text1: 'Dummy Text 1', text2: 'Dummy Text 2'},
-        {id: 3, name: 'C', text1: 'Dummy Text 1', text2: 'Dummy Text 2'}
-      ]
+      columns: []
     }
   },
   components: {
     Section
   },
   methods: {
-    saveDataToFirebase: function ()
-    {
-      this.ajaxRequest = true;
-      this.$http.post('https://vuejs-db-74a8b.firebaseio.com/posts.json', {
-        id: 2, 
-        name: 'Bangalore', 
-        description: `Bangalore (/bæŋɡəˈlɔːr/), officially known as Bengaluru[13] ([ˈbeŋɡəɭuːɾu] (About this sound listen)), is the capital of the Indian state of Karnataka. It has a population of over ten million,[14] making it a megacity and the third most populous city and fifth most populous urban agglomeration in India.[15] It is located in southern India on the Deccan Plateau. Its elevation is over 900 m (3,000 ft) above sea level, the highest of India's major cities.`, 
-        state: 'Karnataka',
-        }, 
+    saveDataToFirebase: function (data) {
+      this.ajaxRequest = true
+      this.$http.post('https://vuejs-db-74a8b.firebaseio.com/posts.json', data,
         function (data, status, request) {
-              console.log(data)
-              this.ajaxRequest = false;
-          });
+          console.log(data)
+          this.ajaxRequest = false
+        })
+    },
+    getDataFromFireBase: function () {
+      this.$http.get('https://vuejs-db-74a8b.firebaseio.com/posts.json').then(
+      function (data, status, request) {
+        console.log(data)
+        this.columns = data.body['-L1SHPzkN0kLJZDe2Df6'].columns
+      })
     }
+  },
+  created: function () {
+    // this.saveDataToFirebase({columns: [{
+    //   id: 1,
+    //   name: 'Mumbai',
+    //   description: `Mumbai (/mʊmˈbaɪ/; also known as Bombay, the official name until 1995) is the capital city of the Indian
+    //   state of Maharashtra. It is the most populous city in India with an estimated city proper population of 12.4 million as of
+    //   2011. Along with the neighbouring regions of the Mumbai Metropolitan Region, it is second most populous metropolitan
+    //   area in India, with a population of 21.3 million as of 2016.[12][13][14] Mumbai lies on the Konkan on the west coast of
+    //   India and has a deep natural harbour.`
+    // },
+    // {
+    //   id: 2,
+    //   name: 'Bangalore',
+    //   description: `Bangalore (/bæŋɡəˈlɔːr/), officially known as Bengaluru[13] ([ˈbeŋɡəɭuːɾu] (About this sound listen)), is the
+    //   capital of the Indian state of Karnataka. It has a population of over ten million,[14] making it a megacity and the third most
+    //   populous city and fifth most populous urban agglomeration in India.[15] It is located in southern India on the Deccan
+    //   Plateau. Its elevation is over 900 m (3,000 ft) above sea level, the highest of India's major cities.`,
+    //   state: 'Karnataka'
+    // },
+    // {
+    //   id: 3,
+    //   name: 'Chennai',
+    //   description: `Chennai (/ˈtʃɛnaɪ/ (About this sound listen); formerly known as Madras /məˈdrɑːs/ (About this sound listen)
+    //   or /-ˈdræs/[11]) is the capital of the Indian state of Tamil Nadu. Located on the Coromandel Coast off the Bay of Bengal,
+    //   it is one of the biggest cultural, economic and educational centres in South India. According to the 2011 Indian census, it is
+    //   the fifth-largest city and fourth-most populous urban agglomeration in India. The city together with the adjoining regions
+    //   constitute the Chennai Metropolitan Area, which is the 36th-largest urban area by population in the world.[12] Chennai is
+    //   among the most visited Indian cities by foreign tourists. It was ranked 43rd most visited city in the world for year
+    //   2015.[13] The Quality of Living Survey rated Chennai as the safest city in India.[14] Chennai attracts 45 percent of health tourists
+    //   visiting India, and 30 to 40 percent of domestic health tourists.[15] As such, it is termed "India's health capital". As a growing
+    //   metropolitan city in a developing country, Chennai confronts substantial pollution and other logistical and socio-economic
+    //   problems.`,
+    //   state: 'Tamil Nadu'
+    // }
+    // ]})
+    this.getDataFromFireBase()
   }
 }
 </script>
--- learn_vueJS1/src/components/Section.vue
+++ learn_vueJS1/src/components/Section.vue
@@ -4,29 +4,7 @@
             {{column.name}}
         </div>
         <p class="text">
-            Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been 
-            the industry's standard dummy text ever since 
-            the 1500s, when an unknown printer took a galley 
-            of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the 
-            leap into electronic typesetting, remaining essentially 
-            unchanged. It was popularised in the 1960s with the release 
-            of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software 
-            like Aldus PageMaker including versions of Lorem Ipsum.    
-            Why do we use it?
-            It is a long established fact that a reader will be 
-            distracted by the readable content of a page when looking at 
-            its layout. The point of using Lorem Ipsum is that 
-            it has a more-or-less normal distribution of letters, 
-            as opposed to using 'Content here, content here', 
-            making it look like readable English. 
-            Many desktop publishing packages and web page editors now 
-            use Lorem Ipsum as their default model text, and a search for 
-            'lorem ipsum' will uncover many web sites still in their infancy. 
-            Various versions have evolved over the years, sometimes by accident,
-            sometimes on purpose (injected humour and the like).        
+            {{column.description}}        
         </p>
     </div>
 </template>
@@ -39,10 +17,10 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  },
-  created: function () {
-    console.log(this.column)
   }
+//   created: function () {
+//     console.log(this.column)
+//   }
 }
 </script>
 
